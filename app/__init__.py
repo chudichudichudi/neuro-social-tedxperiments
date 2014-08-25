0cf296ad61b6b96e426b5ef849e7381b487f8441
chudi <elchudi@gmail.com>
2014-08-25 14:46:51 -0300

    lol

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,15 @@ from flask.ext.social import Social, SQLAlchemyConnectionDatastore, \
      login_failed
 from flask.ext.social.utils import get_connection_values_from_oauth_response
 from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug.contrib.fixers import ProxyFix
 
 from .helpers import Flask
 
 app = Flask(__name__)
+
+
 app.config.from_yaml(app.root_path)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
 db = SQLAlchemy(app)
